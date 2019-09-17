1d4f6c5a660fab8537689169a0e33c2725595e75
Rachel Grier <rgrier104@gmail.com>
2019-09-17 16:47:46 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,4 +4,16 @@ class SchoolClassesController < ApplicationController
 
   def show
   end
+
+  def new
+    @school_classes = SchoolClass.new
+  end
+
+  def create
+    @student = Student.new
+    @student.first_name = params[:first_name]
+    @student.last_name = params[:last_name]
+    @student.save
+    redirect_to student_path(@student)
+  end
 end
