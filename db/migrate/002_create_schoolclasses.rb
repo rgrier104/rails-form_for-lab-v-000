5bfe114792aad76607476017802bd93580f9ff7d
Rachel Grier <rgrier104@gmail.com>
2019-09-17 16:35:07 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_schoolclasses.rb
+++ db/migrate/002_create_schoolclasses.rb
@@ -1,4 +1,4 @@
-class CreateSchoolclasses < ActiveRecord::Migration
+class CreateSchoolClasses < ActiveRecord::Migration
   def change
     create_table :school_classes do |t|
       t.string :title
