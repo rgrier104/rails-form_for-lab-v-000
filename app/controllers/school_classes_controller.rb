0426b97d53873266da3e27d8fe752ed272a448b0
Rachel Grier <rgrier104@gmail.com>
2019-09-17 17:56:10 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -11,7 +11,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(school_class_params)
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.title = params[:title]
     @school_class.room_number = params[:room_number]
     @school_class.save
