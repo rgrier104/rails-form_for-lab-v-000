b592e0d57c2d76e5d3fef7646b747c114cd37792
Rachel Grier <rgrier104@gmail.com>
2019-09-17 18:22:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -8,7 +8,7 @@ class StudentsController < ApplicationController
   end
 
   def new
-    @student = Student.new(student_params(:first_name, :last_name))
+    @student = Student.new
   end
 
   def create
